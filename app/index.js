4e620c7b605867fe75f8525a3546360c06877d85
Felipe <felipe.puziol@gmail.com>
2024-03-13 00:02:16 +0000

    feat: add body-parser as dependency and create auth router

--- app/index.js
+++ app/index.js
@@ -1,30 +1,16 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/auth');
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app
+  .use(bodyParser.json())
+  .use('/auth', authRoutes)
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
-
-app.get('/login', (req, res) => {
-  res.send('TODO')
-})
-
-app.post('/login', (req, res) => {
-  const username = req.body.username
-  const password = req.body.password
-})
-
-
-app.get('/register', (req, res) => {
-  res.send('TODO')
-})
-
-app.post('/register', (req, res) => {
-  const username = req.body.username
-  const password = req.body.password
-  const validationPassword = req.body.validationPassword
 })
 
 app.listen(port, () => {
